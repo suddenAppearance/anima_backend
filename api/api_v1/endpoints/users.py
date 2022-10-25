3400b6b16153f023823502590cf62eb2f25dc70b
sudden_appearance <kuz.damir.01@gmail.com>
2022-10-26 00:22:48 +0300

    init backend service

--- api/api_v1/endpoints/users.py
+++ api/api_v1/endpoints/users.py
@@ -1,11 +1,36 @@
 from fastapi import APIRouter, Depends
+from fastapi.requests import Request
+from fastapi.responses import RedirectResponse
 
 from api.api_v1.deps import get_token
 from services.users import UsersService
 
 router = APIRouter()
 
+# @router.get("/")
+# async def get_current_user(token: str = Depends(get_token)):
+#     return UsersService().get_user_by_token(token)
+code_verifier = None
+
 
 @router.get("/")
-async def get_current_user(token: str = Depends(get_token)):
-    return UsersService().get_user_by_token(token)
+async def redirect_to_login_page(request: Request):
+    global code_verifier
+    url, code_verifier = UsersService().get_login_page_and_code_verifier("http://0.0.0.0:8000/api/v1/users/redirect/")
+    return RedirectResponse(url)
+
+
+@router.get("/login/")
+async def get_login_url(redirect_url: str):
+    return UsersService().get_login_page_and_code_verifier(redirect_url)
+
+
+@router.get("/callback/")
+async def get_token(redirect_url: str, code: str, code_verifier: str):
+    return await UsersService().get_token_by_code(code=code, code_verifier=code_verifier, redirect_url=redirect_url)
+
+
+@router.get('/redirect/')
+async def get_code(request: Request, code: str):
+    return await UsersService().get_token_by_code(redirect_url="http://0.0.0.0:8000/api/v1/users/redirect/",
+                                                  code_verifier=code_verifier, code=code)
--- services/users.py
+++ services/users.py
@@ -1,6 +1,11 @@
 import logging
+import traceback
+from urllib.parse import urlparse
 
-from keycloak import KeycloakOpenID, KeycloakGetError
+import pkce
+import urllib3
+from fastapi import HTTPException
+from keycloak import KeycloakOpenID, KeycloakGetError, KeycloakPostError
 
 from core.settings import Settings
 
@@ -9,3 +14,34 @@ keycloak = KeycloakOpenID(
     realm_name=Settings().KEYCLOAK_REALM,
     client_id='account-console'
 )
+
+
+class UsersService:
+    def get_login_page_and_code_verifier(self, redirect_url):
+        code_verifier = pkce.generate_code_verifier(length=128)
+        code_challenge = pkce.get_code_challenge(code_verifier)
+        auth_endpoint = urlparse(keycloak.well_known()["authorization_endpoint"].replace("keycloak:8080", "localhost:8787")).geturl()
+        return (
+            f"{auth_endpoint}?"
+            f"client_id=account-console&"
+            f"response_type=code&"
+            f"redirect_uri={redirect_url}&"
+            f"scope=email&"
+            f"code_challenge={code_challenge}&"
+            f"code_challenge_method=S256"
+        ), code_verifier
+
+    async def get_token_by_code(self, code: str, redirect_url: str, code_verifier: str):
+        try:
+            access_token = keycloak.token(
+                grant_type="authorization_code",
+                code=code,
+                redirect_uri=redirect_url,
+                code_verifier=code_verifier,
+                client_id="account-console",
+            )
+        except KeycloakPostError as e:
+            logging.getLogger().info(f"Error occured:\n {e}\n {traceback.format_exc()}")
+            raise HTTPException(status_code=404, detail={"message": "Not found"})
+
+        return access_token  # почему-то не хочет подтверждаться
